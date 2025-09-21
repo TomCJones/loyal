17a5768daa7f8cef41e3b321d25eccf2c1033943
tom jones <TomCJones@users.noreply.github.com>
2025-09-21 16:31:44 -0700

    Update server.js
    fix typos

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 require('dotenv').config();
-const loyaltyRoutes = require('./routes/loyaltyRoutes');
+const loyalRoutes = require('./routes/loyalRoutes');
 
 const app = express();
 app.use(express.json());
-app.use('/loyalty', loyaltyRoutes);
+app.use('/loyal', loyalRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
